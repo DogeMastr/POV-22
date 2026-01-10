90dd9fecc91e85b402806e2d0eeb6046a4edc30a
augustinjanuary <0701brian04@gmail.com>
2026-01-10 15:28:40 +0000

    Audio for the car wheels!

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody3D
 
 @onready var collision = $CollisionShape3D
 @onready var cam = $Camera3D
-@onready var left_wheel = $Wheel_SFX/AudioStreamPlayer3D
+@onready var left_wheel = $Wheel_SFX/Left_Wheel
+@onready var right_wheel = $Wheel_SFX/Right_Wheel
 
 @onready var skid_sfx = preload("res://Assets/sounds/skid.wav")
 
@@ -13,18 +14,37 @@ var size_of_street = 5.88
 
 var left_wheel_playing = false
 var right_wheel_playing = false
+var default_wheel_db
 
 func _ready() -> void:
 	print(size_of_street/2 - collision.shape.size.x)
+	default_wheel_db = left_wheel.volume_db
 
 func _process(delta: float) -> void:
 	var hori_input = Input.get_axis("move_left","move_right")
 	if hori_input != 0:
 		velocity.x += speed * hori_input * delta
 		
-	if velocity.x > 0 and hori_input == -1:
-		left_wheel.play()
-		
+	if velocity.x >= 0 and hori_input == -1:
+		if not left_wheel_playing:
+			left_wheel.volume_db = default_wheel_db
+			left_wheel.play()
+			left_wheel_playing = true
+	elif hori_input != -1 and left_wheel_playing:
+		var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_LINEAR)
+		tween.tween_property(left_wheel, "volume_db", -80.0, .5)
+		left_wheel_playing = false
+	
+	if velocity.x <= 0 and hori_input == 1:
+		if not right_wheel_playing:
+			right_wheel.volume_db = default_wheel_db
+			right_wheel.play()
+			right_wheel_playing = true
+	elif hori_input != 1 and right_wheel_playing:
+		var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_LINEAR)
+		tween.tween_property(right_wheel, "volume_db", -80.0, .5)
+		right_wheel_playing = false
+	
 	velocity.x = clampf(velocity.x, -7.0, 7)
 	velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 	
@@ -37,3 +57,6 @@ func _process(delta: float) -> void:
 	cam.rotate_object_local(Vector3.FORWARD, clampf(velocity.x/50, deg_to_rad(-10), deg_to_rad(10)))
 	
 	move_and_slide()
+
+func _on_left_wheel_finished() -> void:
+	left_wheel_playing = false
