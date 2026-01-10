a2c2c122818a9a44050cd4c3a1ea48a55d87d2cb
augustinjanuary <0701brian04@gmail.com>
2026-01-10 20:17:02 +0000

    Update to hand

--- Scripts/player_hand.gd
+++ Scripts/player_hand.gd
@@ -7,21 +7,21 @@ var set_position
 var screen_size
 var lerp_strength = 5.0
 
+var is_dead = false
+
 func _ready() -> void:
 	set_position = container.global_position
 	screen_size = get_viewport().get_visible_rect().size
-	
-func _input(event):
-	if event is InputEventMouseButton:
-		print("Mouse click at: ", event.position)
-		print(container.global_position)
-		print(lerp(container.global_position, event.position, lerp_strength))
+	EventBus.is_dead.connect(on_death)
 
 func _process(delta: float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
-	if mouse_pos.x >= screen_size.x * .45:
+	if mouse_pos.x >= screen_size.x * .45 and not is_dead:
 		container.global_position = container.global_position.slerp(mouse_pos, lerp_strength * delta)
 	else:
 		container.global_position = container.global_position.slerp(set_position, lerp_strength * delta)
 	
 	container.global_position.y = clampf(container.global_position.y, screen_size.y/2, screen_size.y)
+
+func on_death():
+	is_dead = true
