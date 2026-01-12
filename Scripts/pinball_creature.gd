0941fd9d38fb51bd7e2376b20e54a4177205dffe
DogeMastr <mastrdoge@gmail.com>
2026-01-12 15:13:56 +0000

    Gif Addon + glorbo implementation progress

--- Scripts/pinball_creature.gd
+++ Scripts/pinball_creature.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
 var animation_frame_count = 0
-var health = 9;
+var health = 1;
 func _process(delta: float) -> void:
 	animation_frame_count += 1;
 	position.x += cos(animation_frame_count*delta)/30;
@@ -18,8 +18,41 @@ func gotHit():
 
 	flash_tween.tween_property($Sprite2D, "modulate", Color(1, 1, 1), 0.3)
 	health -= 1
+	
+	if health == 0:
+		$AnimatedSprite2D.visible = true
+		$AnimatedSprite2D.play()
+		$CollisionShape2D.set_deferred("disabled", true)
+		$Sprite2D.visible = false
+		$CollisionShape2D.disabled = true
+	
 	if health % 3 == 0:
 		if $Sprite2D.frame != 3:
 			$Sprite2D.frame += 1
 		else:
-			queue_free()
+			print("i should be dead by now")
+
+
+func _on_animated_sprite_2d_animation_finished(anim_name: StringName) -> void:
+	print(anim_name)
+	$AnimatedSprite2D.stop()
+	$RespawnTimer.start()
+	$CollisionShape2D.set_deferred("disabled", true)
+	$AnimatedSprite2D.visible = false
+	$Sprite2D.visible = false
+
+
+func _on_respawn_timer_timeout() -> void:
+	$CollisionShape2D.set_deferred("disabled", false)
+	$Sprite2D.visible = true
+	$RespawnTimer.stop()
+	
+	health = 9
+	$Sprite2D.frame = 0
+	$AnimatedSprite2D.visible = true
+	$AnimatedSprite2D.play_backwards("gif")
+
+func _on_animated_sprite_2d_animation_looped() -> void:
+	$AnimatedSprite2D.visible = false
+	if health == 0:
+		_on_animated_sprite_2d_animation_finished("Looped")
